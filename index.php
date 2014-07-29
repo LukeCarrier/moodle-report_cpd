4e38199d3dc4d913797e900f3cab6376d81a5b07
Luke Carrier <luke@carrier.im>
2014-07-29 16:30:43 +0100

    Luke used consistent on includes...
    ...it was SUPER EFFECTIVE

--- index.php
+++ index.php
@@ -24,13 +24,14 @@
  */
 
 
-require_once('../../config.php');
-require_once($CFG->libdir.'/adminlib.php');
-require_once($CFG->dirroot.'/lib/tablelib.php');
-require_once "$CFG->libdir/formslib.php";
-require_once('cpd_filter_form.php');
-require_once('lib.php');
+require_once dirname(dirname(__DIR__)) . '/config.php';
 
+require_once "{$CFG->libdir}/adminlib.php";
+require_once "{$CFG->libdir}/tablelib.php";
+require_once "{$CFG->libdir}/formslib.php";
+
+require_once __DIR__ . '/cpd_filter_form.php';
+require_once __DIR__ . '/lib.php';
 
 // Check permissions.
 require_login($SITE);
